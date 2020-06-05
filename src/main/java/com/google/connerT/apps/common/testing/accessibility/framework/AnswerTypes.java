8d79bc906a1c1401b06dc6cb7491db19bac654c0
Conner Tolley <jim.tolley@hcahealthcare.com>
2020-06-05 17:48:46 -0500

    change package name

---
+++ src/main/java/com/google/connerT/apps/common/testing/accessibility/framework/AnswerTypes.java
@@ -0,0 +1,11 @@
+package com.google.connerT.apps.common.testing.accessibility.framework;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+import com.google.common.annotations.Beta;
+
+/** Container class for answer types */
+@Beta
+public final class AnswerTypes {
+  private AnswerTypes() {}
+}
---
+++ src/main/java/com/google/connerT/apps/common/testing/accessibility/framework/QuestionTypes.java
@@ -0,0 +1,9 @@
+package com.google.connerT.apps.common.testing.accessibility.framework;
+
+import com.google.common.annotations.Beta;
+
+/** Container class for instances of {@link QuestionType} */
+@Beta
+public final class QuestionTypes {
+  private QuestionTypes() {}
+}
